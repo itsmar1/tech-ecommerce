fa3c3ed4eb88726faad2c42238960897ba6b81f6
Marouane Ahal <d144013152@gmail.com>
2022-03-22 01:11:04 +0100

    create about page

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,1 +1,3 @@
 export const HERO_URL = 'https://tech-ecommerce.s3.eu-west-2.amazonaws.com/images/hero2.png';
+
+export const ABOUT_IMG_URL = 'https://tech-ecommerce.s3.eu-west-2.amazonaws.com/images/hero1.png';
