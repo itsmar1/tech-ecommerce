0b5dbd22bedd91dd35d9d22de6a690e9d5645c6f
Marouane Ahal <d144013152@gmail.com>
2022-04-04 17:02:49 +0000

    feat: redux-persist cart data

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
 import "./index.css";
 import App from "./App";
 import store from "./store";
 
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,14 +5,40 @@ import productsSlice from './products-slice';
 import authSlice from './auth-slice';
 import uiSlice from './ui-slice';
 
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+  import storage from 'redux-persist/lib/storage';
+
+  const cartPersistConfig = {
+    key: 'cart',
+    version: 1,
+    storage,
+  }
+
+  const cartReducer = persistReducer(cartPersistConfig, cartSlice.reducer);
+
+
 
 const store = configureStore({
     reducer: {
-        cart: cartSlice.reducer,
+        cart: cartReducer,
         products: productsSlice.reducer,
         auth: authSlice.reducer,
         ui: uiSlice.reducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 
