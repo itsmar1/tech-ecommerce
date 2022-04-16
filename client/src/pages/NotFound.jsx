6c5847bb4aa5f71fa8f06b60428387700f39a7f4
Marouane Ahal <d144013152@gmail.com>
2022-04-16 00:19:07 +0000

    page not found styling

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
+
 
 const Error = () => {
-    return <h1>
-        Error Page
-    </h1>
+    return (
+        <div className='h-screen w-full bg-secondary-200'>
+            <div className='h-full flex flex-col justify-center items-center text-center my-auto mx-auto'>
+                <h1 className='text-[11rem] text-primary tracking-widest font-extrabold'>404</h1>
+                <h2 className='text-4xl font-semibold text-primary'>Ooops! Sorry, Page Not Found</h2>
+            </div>
+        </div>
+    );
 };
+
 
 export default Error;
